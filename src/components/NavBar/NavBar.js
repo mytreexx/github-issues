fad5b1536f02f5898e6e02e8a1e56f5f07455758
Tali Volf <tali.vulf@gmail.com>
2020-01-23 18:41:19 +0200

    Add basic layout to NavBar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,10 +1,44 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Flex } from 'reflexbox/styled-components';
 
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <StyledNavBar>
+      <div>
+        <a>
+          logo
+        </a>
+        <input />
+        <a href='/'>Pull Requests</a>
+        <a href='/'>Issues</a>
+        <a href='/'>Marketplace</a>
+        <a href='/'>Explore</a>
+      </div>
+      <div>
+        <a href='/'>notifications</a>
+        <a href='/'>plus</a>
+        <a href='/'>profile</a>
+      </div>
+    </StyledNavBar>
   );
 }
+
+
+const StyledNavBar = styled(Flex).attrs({
+  as: "header",
+  width: "100%",
+  height: "64px",
+  alignItems: "center",
+})`
+  background-color: #24292E;
+
+  a {
+    margin: 0;
+    color: #ffffff;
+    text-decoration: none;
+  }
+`;
 
 export default NavBar;
