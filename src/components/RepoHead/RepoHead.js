872a02bd990265830e0dd3cf49c971dbc278bd89
Tali Volf <tali.vulf@gmail.com>
2020-01-25 13:08:03 +0200

    Basic layout for ReoHead

--- src/components/RepoHead/RepoHead.js
+++ src/components/RepoHead/RepoHead.js
@@ -1,14 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Flex } from 'reflexbox/styled-components';
+
 import RepoNavBar from './RepoNavBar/RepoNavBar';
 
 
 const RepoHead = () => {
   return (
-    <>
-      <div>RepoHead</div>
+    <StyledRepoHead>
+      <div>Username/Repository</div>
       <RepoNavBar />
-    </>
+    </StyledRepoHead>
   );
 }
+
+
+const StyledRepoHead = styled(Flex).attrs({
+  as: "header",
+  width: "100%",
+  height: "112px",
+  alignItems: "center",
+  justifyContent: "center",
+})`
+  background-color: #FAFBFC;
+  padding-top: 24px;
+  border-bottom: solid 1px #e1e4e8;  
+`;
 
 export default RepoHead;
