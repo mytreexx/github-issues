51532a06fe1c38b9cf13c0e12ad28bf145fdff9d
Tali Volf <tali.vulf@gmail.com>
2020-01-25 13:07:36 +0200

    Basic layout for IssueContainer

--- src/components/IssueContainer/IssueContainer.js
+++ src/components/IssueContainer/IssueContainer.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Flex } from 'reflexbox/styled-components';
+
 import IssueContainerNav from './IssueContainerNav/IssueContainerNav';
 
 
@@ -6,9 +9,18 @@ const IssueContainer = () => {
   return (
     <>
       <IssueContainerNav />
-      <div>Issue Container</div>
+      <StyledIssueContainer />
     </>
   );
 }
+
+const StyledIssueContainer = styled(Flex).attrs({
+  width: "978px",
+  height: "335px",
+})`
+  padding-top: 24px;
+  border: solid 1px #d1d5da;
+  margin: auto; 
+`;
 
 export default IssueContainer;
