ea6a8dc51b75fe438cc07c134decdeeacd123627
Tali Volf <tali.vulf@gmail.com>
2020-01-25 18:11:17 +0200

    Create Main.js

---
+++ src/components/Main/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import RepoHead from './RepoHead/RepoHead';
+import IssueContainer from './IssueContainer/IssueContainer';
+
+
+const Main = () => {
+  return (
+    <>
+      <RepoHead />
+      <IssueContainer />
+    </>
+  );
+}
+
+export default Main;
