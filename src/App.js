30a636014c9ebc6f5bc03e620c4ee2d73bc21286
Tali Volf <tali.vulf@gmail.com>
2020-01-25 14:00:24 +0200

    Add footer to App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import NavBar from './components/NavBar/NavBar';
 import RepoHead from './components/RepoHead/RepoHead';
 import IssueContainer from './components/IssueContainer/IssueContainer';
+import Footer from './components/Footer/Footer';
 
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
       <NavBar />
       <RepoHead />
       <IssueContainer />
+      <Footer />
     </>
   );
 }
