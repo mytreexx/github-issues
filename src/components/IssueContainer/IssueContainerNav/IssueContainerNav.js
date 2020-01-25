975f7b43e754c7e071b86a3098b0371b195eae4e
Tali Volf <tali.vulf@gmail.com>
2020-01-25 13:08:15 +0200

    Basic layout for IssueContainerNav

--- src/components/IssueContainer/IssueContainerNav/IssueContainerNav.js
+++ src/components/IssueContainer/IssueContainerNav/IssueContainerNav.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Flex } from 'reflexbox/styled-components';
 
 
 const IssueContainerNav = () => {
   return (
-    <div>Issue Container Nav</div>
+    <StyledIssueContainerNav>
+      <button>Filters</button>
+      <input></input>
+      <button>Lables</button>
+      <button>Milestones</button>
+      <button>New issue</button>
+    </StyledIssueContainerNav>
   );
 }
+
+const StyledIssueContainerNav = styled(Flex).attrs({
+  width: "978px",
+  margin: "auto",
+  marginBottom: "16px",
+})`
+  padding-top: 24px;
+
+`;
+
 
 export default IssueContainerNav;
