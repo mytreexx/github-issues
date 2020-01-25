6f1f8b2059a7bcec631c940c14dde30e1cb12ec7
Tali Volf <tali.vulf@gmail.com>
2020-01-25 14:01:44 +0200

    Add temporary content to issues container

--- src/components/IssueContainer/IssueContainer.js
+++ src/components/IssueContainer/IssueContainer.js
@@ -9,7 +9,16 @@ const IssueContainer = () => {
   return (
     <>
       <IssueContainerNav />
-      <StyledIssueContainer />
+      <StyledIssueContainer>
+        <h1>
+          Welcome to Issues!
+        </h1>
+        <p>
+          Issues are used to track todos, bugs, feature requests, and more. As issues are created, they’ll appear here in
+          <br />
+          a searchable and filterable list. To get started, you should create an issue.
+        </p>
+      </StyledIssueContainer>
     </>
   );
 }
@@ -20,7 +29,11 @@ const StyledIssueContainer = styled(Flex).attrs({
 })`
   padding-top: 24px;
   border: solid 1px #d1d5da;
+  border-radius: 3px;
   margin: auto; 
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 export default IssueContainer;
