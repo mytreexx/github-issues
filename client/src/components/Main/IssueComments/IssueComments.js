98ba7d6378ecb2035b7f40fb21f73931565bd99c
Tali Volf <tali.vulf@gmail.com>
2020-03-15 22:27:16 +0200

    fetch issue comments data in IssueComments and save to state

--- client/src/components/Main/IssueComments/IssueComments.js
+++ client/src/components/Main/IssueComments/IssueComments.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 
 const IssueComments = () => {
+  const [issue, setIssue] = useState();
+  const [issueComments, setIssueComments] = useState();
+
+  useEffect(() => {
+    fetch('http://localhost:8000/repos/bluzi/name-db/458')
+      .then(response => response.json())
+      .then(issueDetails => {
+        setIssue(issueDetails.items);
+        console.log(issueDetails.title);
+      })
+  }, []);
+
+  useEffect(() => {
+    fetch('http://localhost:8000/repos/bluzi/name-db/458/comments')
+      .then(response => response.json())
+      .then(issueComments => {
+        setIssueComments(issueComments.items);
+        issueComments.map((comment) =>
+        console.log(comment.body));
+      })
+  }, []);
+
+
   return (
-    <>
-        Issue Comments
-    </>
-  );
+    <div>
+      issue comments
+    </div>
+  )
 }
 
 export default IssueComments;
