ff46a57327d8d911516b815bcfb560a532cdb52d
Tali Volf <tali.vulf@gmail.com>
2020-03-18 18:05:09 +0200

    Delete unnecessary import in IssueComments

--- client/src/components/Main/IssueComments/IssueComments.js
+++ client/src/components/Main/IssueComments/IssueComments.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-// import { Flex } from 'reflexbox/styled-components';
 import { useParams } from 'react-router-dom';
 
 
