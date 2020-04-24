fda7a0b2010e7bab253c418395b6f69bdbec0638
Tali Volf <tali.vulf@gmail.com>
2020-04-24 15:13:55 +0300

    Create Welcome component

---
+++ client/src/components/Main/Welcome/Welcome.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ARROW from '../../../assets/images/arrow.png';
+
+
+const Welcome = () => {
+  return (
+    <>
+      <h1>
+        Welcome to Tali's Repository issues!
+      </h1>
+      <img src={ARROW} alt='Logo' />
+    </>
+  );
+}
+
+export default Welcome;
