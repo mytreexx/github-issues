0d53d437d53bb75f597736b3ce0b1ec3ab121512
Tali Volf <tali.vulf@gmail.com>
2020-04-27 13:18:44 +0300

    Create Issues component to nest some of the routes from Main.js

---
+++ client/src/components/Main/Issues/Issues.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+
+import RepoHead from '../RepoHead/RepoHead';
+import IssueContainer from '../IssueContainer/IssueContainer';
+import IssueComments from '../IssueComments/IssueComments';
+
+
+const Issues = () => {
+  return (
+    <>
+      <RepoHead />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/:userName/:repoName/issues" exact component={IssueContainer} />
+          <Route path="/:userName/:repoName/issues/:issueNumber" exact component={IssueComments} />
+        </Switch>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default Issues;
