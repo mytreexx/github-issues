c9240e45df840f7ea56a280594e951e09a5515dd
Tali Volf <tali.vulf@gmail.com>
2020-04-27 11:31:25 +0300

    Style Welcome page

--- client/src/components/Main/Welcome/Welcome.js
+++ client/src/components/Main/Welcome/Welcome.js
@@ -1,16 +1,40 @@
 import React from 'react';
-import ARROW from '../../../assets/images/arrow.png';
+import styled from 'styled-components';
+import Octicon, { LightBulb } from '@primer/octicons-react'
 
 
 const Welcome = () => {
   return (
-    <>
-      <h1>
-        Welcome to Tali's Repository issues!
-      </h1>
-      <img src={ARROW} alt='Logo' />
-    </>
+    <MainContainer>
+      <LightBulbOcticon icon={LightBulb} />
+      <span>
+        <h1>
+          Welcome to the repository issue finder!
+        </h1>
+
+        <p>
+          You can view any repo's issue section by searching it in the navbar.
+        </p>
+      </span>
+    </MainContainer>
   );
 }
+
+
+const LightBulbOcticon = styled(Octicon)`
+  width: 200px;
+  height: 200px;
+  color: #C6CBD1;
+`
+
+const MainContainer = styled.div`
+  display: flex;
+  margin: 75px 0;
+  justify-content: center;
+
+  span{
+    margin-left: 20px;
+  }
+`
 
 export default Welcome;
