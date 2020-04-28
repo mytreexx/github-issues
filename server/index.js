4c44f23d352db63832b17846fc5f3a6b0e6de618
Tali Volf <tali.vulf@gmail.com>
2020-04-28 14:09:24 +0300

    Add error handling to server

--- server/index.js
+++ server/index.js
@@ -8,31 +8,43 @@ const axios = require('axios');
 
 app.use(cors());
 
-app.get('/repos/:userName/:repoName/:issueNumber/comments', async function (req, res) { 
-  const response = await axios.get(`https://api.github.com/repos/${req.params.userName}/${req.params.repoName}/issues/${req.params.issueNumber}/comments`, {
-    headers: {
-      'Authorization': `token ${process.env.ACCESS_TOKEN}`
-    }
-  });
-  res.json(response.data);
+app.get('/repos/:userName/:repoName/:issueNumber/comments', async function (req, res) {
+  try {
+    const response = await axios.get(`https://api.github.com/repos/${req.params.userName}/${req.params.repoName}/issues/${req.params.issueNumber}/comments`, {
+      headers: {
+        'Authorization': `token ${process.env.ACCESS_TOKEN}`
+      }
+    });
+    res.json(response.data);
+  } catch (error) {
+    res.json(error);
+  }
 });
 
-app.get('/repos/:userName/:repoName/:issueNumber', async function (req, res) { 
-  const response = await axios.get(`https://api.github.com/repos/${req.params.userName}/${req.params.repoName}/issues/${req.params.issueNumber}`, {
-    headers: {
-      'Authorization': `token ${process.env.ACCESS_TOKEN}`
-    }
-  });
-  res.json(response.data);
+app.get('/repos/:userName/:repoName/:issueNumber', async function (req, res) {
+  try {
+    const response = await axios.get(`https://api.github.com/repos/${req.params.userName}/${req.params.repoName}/issues/${req.params.issueNumber}`, {
+      headers: {
+        'Authorization': `token ${process.env.ACCESS_TOKEN}`
+      }
+    });
+    res.json(response.data);
+  } catch (error) {
+    res.json(error);
+  }
 });
 
-app.get('/repos/:userName/:repoName/', async function (req, res) { 
-  const response = await axios.get(`https://api.github.com/search/issues?q=repo:${req.params.userName}/${req.params.repoName}/ is:issue is:open`, {
-    headers: {
-      'Authorization': `token ${process.env.ACCESS_TOKEN}`
-    }
-  });
-  res.json(response.data);
+app.get('/repos/:userName/:repoName/', async function (req, res) {
+  try {
+    const response = await axios.get(`https://api.github.com/search/issues?q=repo:${req.params.userName}/${req.params.repoName}/ is:issue is:open`, {
+      headers: {
+        'Authorization': `token ${process.env.ACCESS_TOKEN}`
+      }
+    });
+    res.json(response.data);
+  } catch (error) {
+    res.json(error);
+  }
 });
 
 app.listen(port, () => console.log("Running!"));
