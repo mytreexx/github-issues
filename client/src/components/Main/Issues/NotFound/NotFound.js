7b69e770bc592de10893a4f175dba5ca2f1ee9e2
Tali Volf <tali.vulf@gmail.com>
2020-05-07 21:10:19 +0300

    Add NotFound component

---
+++ client/src/components/Main/Issues/NotFound/NotFound.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import styled from 'styled-components';
+import Octicon, { X } from '@primer/octicons-react'
+
+
+const NotFound = () => {
+  return (
+    <MainContainer>
+      <LightBulbOcticon icon={X} />
+      <span>
+        <h1>
+          Oops!
+        </h1>
+
+        <p>
+          It looks like we can't find the repositiry you're searching for.
+        </p>
+      </span>
+    </MainContainer>
+  );
+}
+
+
+const LightBulbOcticon = styled(Octicon)`
+  width: 200px;
+  height: 200px;
+  color: #C6CBD1;
+`
+
+const MainContainer = styled.div`
+  display: flex;
+  margin: 75px 0;
+  justify-content: center;
+
+  span{
+    margin-left: 20px;
+  }
+`
+
+export default NotFound;
