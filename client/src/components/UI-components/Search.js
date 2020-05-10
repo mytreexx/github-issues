9c414354abd769f16107c19e4da40fc5dccf1e20
Tali Volf <tali.vulf@gmail.com>
2020-05-10 12:20:45 +0300

    Create search component

---
+++ client/src/components/UI-components/Search.js
@@ -0,0 +1,19 @@
+import styled  from "styled-components";
+
+const Search = styled.input`
+  background-color: #3F4448;
+  width: 300px;
+  height: 28px;
+  border-style: none;
+  border-radius: 3px;
+  padding: 0;
+}
+
+::placeholder {
+  color: #8c8f92;
+  font-weight: 600;
+  padding-left: 7px;
+}
+`
+
+export default Search;
