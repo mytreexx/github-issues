beae43b363325452d2acb6b6542380e6ba398511
Tali Volf <tali.vulf@gmail.com>
2020-05-10 12:52:19 +0300

    Add inverse colors to Search component

--- client/src/components/UI-components/Search.js
+++ client/src/components/UI-components/Search.js
@@ -1,7 +1,7 @@
 import styled  from "styled-components";
 
 const Search = styled.input`
-  background-color: #3F4448;
+  background-color: ${props => props.type === "inverse" ? "#C6CBD1" : "#3F4448"};
   width: 300px;
   height: 28px;
   border-style: none;
@@ -11,6 +11,7 @@ const Search = styled.input`
 
 ::placeholder {
   color: #8c8f92;
+  color: ${props => props.type === "inverse" ? "#3F4448" : "#8c8f92"};
   font-weight: 600;
   padding-left: 7px;
 }
