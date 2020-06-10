af7cf98f0fcb70abacf34edf9dd810da150c806c
Tali Volf <tali.vulf@gmail.com>
2020-06-10 16:35:51 +0300

    style repoHead

--- client/src/components/Main/Issues/RepoHead/RepoHead.js
+++ client/src/components/Main/Issues/RepoHead/RepoHead.js
@@ -24,24 +24,38 @@ const RepoHead = () => {
         <RepoHeader>
           <span>
             <StyledOcticon icon={Repo} />
-            bluzi/name-db
+            <RepoTitle>
+              aseprite / <span>aseprite</span>
+            </RepoTitle>
           </span>
 
-          <span>
-            <Button>
-              <Octicon icon={Eye} />
-              Watch
-              <Octicon icon={TriangleDown} />
-            </Button>
-            <Button>
-              <Octicon icon={Star} />
-              Star
-            </Button>
-            <Button>
-              <Octicon icon={RepoForked} />
-              Fork
-            </Button>
-          </span>
+          <SideButtons>
+            <MenuButton>
+              <button>
+                <StyledOcticon type='dark' icon={Eye} />
+                Watch
+                <StyledOcticon type='dark' icon={TriangleDown} />
+              </button>
+              <div>309</div>
+            </MenuButton>
+
+            <MenuButton>
+              <button>
+                <StyledOcticon type='dark' icon={Star} />
+                Star
+              </button>
+              <div>9.1k</div>
+            </MenuButton>
+
+            <MenuButton>
+              <button>
+                <StyledOcticon type='dark' icon={RepoForked} />
+                Fork
+              </button>
+              <div>934</div>
+            </MenuButton>
+
+          </SideButtons>
         </RepoHeader>
 
         <RepoNavbar>
@@ -50,12 +64,12 @@ const RepoHead = () => {
             Code
           </Tab>
           <SelectedTab>
-            <StyledOcticon icon={IssueOpened} />
-            Issues 17
+            <StyledOcticon type='dark' icon={IssueOpened} />
+            Issues <span>829</span>
           </SelectedTab>
           <Tab>
             <StyledOcticon icon={GitPullRequest} />
-            Pull Requests 95
+            Pull Requests <span>5</span>
           </Tab>
           <Tab>
             <StyledOcticon icon={Play} />
@@ -63,7 +77,7 @@ const RepoHead = () => {
           </Tab>
           <Tab>
             <StyledOcticon icon={Shield} />
-            Security
+            Security <span>0</span>
           </Tab>
           <Tab>
             <StyledOcticon icon={Graph} />
@@ -80,12 +94,12 @@ const RepoHead = () => {
 const Container = styled(Flex).attrs({
   as: "header",
   width: "100%",
-  height: "112px",
+  height: "108px",
   alignItems: "center",
   justifyContent: "center",
 })`
   background-color: #FAFBFC;
-  padding-top: 24px;
+  padding-top: 16px;
   border-bottom: solid 1px #e1e4e8;  
 `;
 
@@ -104,6 +118,17 @@ const RepoHeader = styled.div`
   width: 978px;
 `
 
+const RepoTitle = styled.span`
+  color: #0366D6;
+  text-decoration: none;
+  font-size: 18px;
+  margin-left: 8px;
+
+  span {
+    font-weight: 600;
+  }
+`
+
 const RepoNavbar = styled.div`
   width: 100%;
 `
@@ -118,6 +143,15 @@ const Tab = styled.div`
     color: #24292e;
     cursor: pointer;
   }
+
+  span {
+    color: #444d56;
+    font-size: 12px;
+    font-weight: 600;
+    background-color: rgba(27,31,35,.08);
+    border-radius: 20px;
+    padding: 0 4px;
+  }
 `
 
 const SelectedTab = styled(Tab)`
@@ -130,15 +164,58 @@ const SelectedTab = styled(Tab)`
   `
 
 const StyledOcticon = styled(Octicon)`
-  color: rgba(27,31,35,.3);
+  color: ${props => props.type === 'dark' ? '#24292e' : 'rgba(27,31,35,.3)'};
   margin-right: 4px;
+  margin-left: ${props => props.icon === TriangleDown && '4px'};  
+  width: ${props => props.icon === TriangleDown && '8px'};  
+
 `
 
-const Button = styled.button`
+const MenuButton = styled.div`
+  height: 28px;
+  box-sizing: border-box;
+  margin-left: 10px;
   font-size: 12px;
-  font-weight: 600;
-  color: #24292e;
+  line-height: 27px;
   
+
+  button {
+    background-image: linear-gradient(-180deg,#fafbfc,#eff3f6 90%);
+    border: none;
+    padding: 3px 10px;
+    font-size: 12px;
+    font-weight: 600;
+    color: #24292e;
+    height: 100%;
+    border: 1px solid rgba(27,31,35,.2);
+    border-radius: 3px 0 0 3px;
+      
+    :hover {
+      background-image: none;
+      background-color: #E6EBF1;
+      border: 1px solid #A0A5AA;
+    }
+  }
+
+  div {
+    display: inline-block;
+    padding: 0 10px;
+    background-color: white;
+    font-weight: 600;
+    border: 1px solid rgba(27,31,35,.2);
+    border-radius: 0 3px 3px 0;
+    border-left: 0;
+    height: 100%;
+    box-sizing: border-box;
+    
+    :hover {
+      color: #0366D6;
+    }
+  }
+`
+
+const SideButtons = styled.span`
+  display: flex;
 `
 
 export default RepoHead;
