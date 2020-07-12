4e2ab871bde781e6501b1201f815c19570eaa8c9
Tali Volf <tali.vulf@gmail.com>
2020-07-12 18:12:17 +0300

    Add usage for NotFound component in main routing

--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -3,15 +3,17 @@ import { Switch, Route } from 'react-router-dom';
 
 import Welcome from './Welcome/Welcome';
 import Issues from './Issues/Issues';
+import NotFound from '../UI-components/NotFound';
 
 
 const Main = () => {
   return (
-     
-        <Switch>
-          <Route path="/" exact component={Welcome} />
-          <Route path="/:userName/:repoName/issues" component={Issues} />
-        </Switch>
+
+    <Switch>
+      <Route path="/" exact component={Welcome} />
+      <Route path="/:userName/:repoName/issues" component={Issues} />
+      <Route path="/" component={NotFound} />
+    </Switch>
   );
 }
 
