326966d91f40ab77af4990ebbc10f5c0ddf94426
Tali Volf <tali.vulf@gmail.com>
2020-07-13 16:57:10 +0300

    Refactor Footer to be responsive

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -6,44 +6,36 @@ import Octicon, { MarkGithub } from '@primer/octicons-react'
 const Footer = () => {
   return (
     <FooterContainer>
-      <Container>
-        <span>© 2020 GitHub, Inc.</span>
-        <a href='https://github.com/site/terms'>Terms</a>
-        <a href='https://github.com/site/privacy'>Privacy</a>
-        <a href='https://github.com/security'>Security</a>
-        <a href='https://githubstatus.com/'>Status</a>
-        <a href='https://help.github.com/'>Help</a>
-      </Container>
+      <span>© 2020 GitHub, Inc.</span>
+      <a href='https://github.com/site/terms'>Terms</a>
+      <a href='https://github.com/site/privacy'>Privacy</a>
+      <a href='https://github.com/security'>Security</a>
+      <a href='https://githubstatus.com/'>Status</a>
+      <a href='https://help.github.com/'>Help</a>
 
       <a href='https://github.com/'><GithubLogo icon={MarkGithub} /></a>
-      
-      <Container>
-        <a href='https://github.com/contact'>Contact GitHub</a>
-        <a href='https://github.com/pricing'>Pricing</a>
-        <a href='https://developer.github.com/'>API</a>
-        <a href='https://training.github.com/'>Training</a>
-        <a href='https://github.blog/'>Blog</a>
-        <a href='https://github.com/about'>About</a>
-      </Container>
-    </FooterContainer>
 
+      <a href='https://github.com/contact'>Contact GitHub</a>
+      <a href='https://github.com/pricing'>Pricing</a>
+      <a href='https://developer.github.com/'>API</a>
+      <a href='https://training.github.com/'>Training</a>
+      <a href='https://github.blog/'>Blog</a>
+      <a href='https://github.com/about'>About</a>
+    </FooterContainer>
   );
 }
 
+
 const FooterContainer = styled.div`
-  width: 980px;
+  max-width: 1248px;
+  flex-wrap: wrap;
   margin: auto;
   margin-top: 40px;
-  padding: 40px 0;
-  height: 25px;
+  padding: 40px 32px;
+  min-height: 25px;
   border-top: 1px solid #EAECEF;
   display: flex;
-  justify-content: space-between;
-`
-
-const Container = styled.div`
-  display: flex;
-  justify-content: space-between;
+  justify-content: space-between;  
   color: #586069;
   font-size: 12px;
   
@@ -55,7 +47,6 @@ const Container = styled.div`
     :hover {
       text-decoration: underline;
     }
-  }
 `
 
 const GithubLogo = styled(Octicon)`
