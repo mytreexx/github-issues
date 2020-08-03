6c99b48c0d75883e814ffe8ca9fda965b0b92966
Tali Volf <tali.vulf@gmail.com>
2020-08-03 23:32:28 +0300

    Add query for page number and re-arrange index.js in server

--- server/index.js
+++ server/index.js
@@ -8,6 +8,32 @@ const axios = require('axios');
 
 app.use(cors());
 
+app.get('/repos/:userName/:repoName/labels', async function (req, res) {
+  try {
+    const response = await axios.get(`https://api.github.com/repos/${req.params.userName}/${req.params.repoName}/labels`, {
+      headers: {
+        'Authorization': `token ${process.env.ACCESS_TOKEN}`
+      }
+    });
+    res.json(response.data);
+  } catch (error) {
+    res.json({ error: true });
+  }
+});
+
+app.get('/repos/:userName/:repoName/milestones', async function (req, res) {
+  try {
+    const response = await axios.get(`https://api.github.com/repos/${req.params.userName}/${req.params.repoName}/milestones`, {
+      headers: {
+        'Authorization': `token ${process.env.ACCESS_TOKEN}`
+      }
+    });
+    res.json(response.data);
+  } catch (error) {
+    res.json({ error: true });
+  }
+});
+
 app.get('/repos/:userName/:repoName/:issueNumber/comments', async function (req, res) {
   try {
     const response = await axios.get(`https://api.github.com/repos/${req.params.userName}/${req.params.repoName}/issues/${req.params.issueNumber}/comments`, {
@@ -34,24 +60,12 @@ app.get('/repos/:userName/:repoName/:issueNumber', async function (req, res) {
   }
 });
 
-app.get('/repos/:userName/:repoName/page/:pageNumber', async function (req, res) {
+app.get('/repos/:userName/:repoName', async function (req, res) {
   try {
     const isQuery = req.query.is === 'closed' ? 'is:closed' : req.query.is === 'all' ? '' : 'is:open';
+    const page = Number(req.query.page) || 1;
     console.log(req.query);
-    const response = await axios.get(`https://api.github.com/search/issues?q=repo:${req.params.userName}/${req.params.repoName}/ is:issue ${isQuery} &per_page=25 &page=${req.params.pageNumber}`, {
-      headers: {
-        'Authorization': `token ${process.env.ACCESS_TOKEN}`
-      }
-    });
-    res.json(response.data);
-  } catch (error) {
-    res.json({ error: true });
-  }
-});
-
-app.get('/repos/:userName/:repoName/', async function (req, res) {
-  try {
-    const response = await axios.get(`https://api.github.com/search/issues?q=repo:${req.params.userName}/${req.params.repoName}/ is:issue is:open &per_page=25`, {
+    const response = await axios.get(`https://api.github.com/search/issues?q=repo:${req.params.userName}/${req.params.repoName}/ is:issue ${isQuery} &per_page=25 &page=${page} `, {
       headers: {
         'Authorization': `token ${process.env.ACCESS_TOKEN}`
       }
@@ -65,32 +79,6 @@ app.get('/repos/:userName/:repoName/', async function (req, res) {
 app.get('/:userName/:repoName/', async function (req, res) {
   try {
     const response = await axios.get(`https://api.github.com/repos/${req.params.userName}/${req.params.repoName}`, {
-      headers: {
-        'Authorization': `token ${process.env.ACCESS_TOKEN}`
-      }
-    });
-    res.json(response.data);
-  } catch (error) {
-    res.json({ error: true });
-  }
-});
-
-app.get('/:userName/:repoName/labels', async function (req, res) {
-  try {
-    const response = await axios.get(`https://api.github.com/repos/${req.params.userName}/${req.params.repoName}/labels`, {
-      headers: {
-        'Authorization': `token ${process.env.ACCESS_TOKEN}`
-      }
-    });
-    res.json(response.data);
-  } catch (error) {
-    res.json({ error: true });
-  }
-});
-
-app.get('/:userName/:repoName/milestones', async function (req, res) {
-  try {
-    const response = await axios.get(`https://api.github.com/repos/${req.params.userName}/${req.params.repoName}/milestones`, {
       headers: {
         'Authorization': `token ${process.env.ACCESS_TOKEN}`
       }
