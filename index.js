56f7725ef3879adcfb1758d383969d634a48d5c2
TaliV <tali.vulf@gmail.com>
2021-06-20 18:30:25 +0300

    fix path serving client

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ app.get('/:userName/:repoName/', async function (req, res) {
 });
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../", "client", "build", "index.html"));
+  res.sendFile(path.join(__dirname,"client", "build", "index.html"));
 });
 
 console.log(__dirname)
